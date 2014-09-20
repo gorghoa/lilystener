fb0d983e698e5c655592ecc4357c2b7100d0187f
Rodrigue Villetard <rodrigue.villetard@sacristi.fr>
2014-09-20 19:15:35 +0200

    idle indicator

--- colique.js
+++ colique.js
@@ -1,12 +1,16 @@
 "use strict";
 var colique = {
   hist : [],
+  histStep : [],
   steps : {high:60,medium:10},
+  countIdem:0,
   addIntensity : function(intensity) {
     var hist=this.hist;
     hist.push(parseInt(intensity,10));
 
     var slice = hist.slice(Math.max(hist.length - 5, 1))
+
+
 
     var avg = function(arr) {
         var sum=0;
@@ -20,10 +24,26 @@ var colique = {
     }
 
 
+
     this.paliatedIntensity={intensity:avg(slice),time:Date.now()};
+    this.histStep.push(this.paliatedIntensity.intensity);
 
   },
   getCurrentStep:function() {
+
+    var countIdem=0,last=0,item;
+    var arr = this.hist.slice(Math.max(this.hist.length - 100, 1));
+    for (var i=0;i<arr.length;i++) {
+        item = arr[i];
+        if(last==item) countIdem++;
+        else { countIdem=0 };
+        last=item;
+    }
+
+    if(countIdem>99)  {
+        return {time:Date.now(),step:'seemIdle',idle:countIdem};
+    }
+
     return {time:this.paliatedIntensity.time,step:this.getStep(this.paliatedIntensity.intensity)};
   },
   getStep:function(intensity) {
